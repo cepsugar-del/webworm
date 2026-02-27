6f32a0d292b9da6532568385bb9e4c8dda707d5c
cepsugar-del <cepsugar@gmail.com>
2026-02-26 23:22:22 -0700

    storing scores is now functioning (looks baaad)

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -35,9 +35,8 @@ export function Home() {
         <caption className = "centered"><b>Leaderboard</b></caption>
         <tr>
             <th>Name</th>
-            <th>Games</th>
-            <th>Total</th>
-            <th>Average</th>
+            <th>Points</th>
+            <th>Date</th>
         </tr>
         <tbody id = 'scores'>
             {leaderboard}
