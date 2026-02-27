c2585bc5717f010e9e4f5869e7078ce2536b02a4
cepsugar-del <cepsugar@gmail.com>
2026-02-26 22:49:03 -0700

    please help me

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './src/app';
-
-console.log("Made it into index.jsx");
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
