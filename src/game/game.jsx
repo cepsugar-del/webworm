455ca8b19916779f5988ee3c16fdc92580b3cae1
cepsugar-del <cepsugar@gmail.com>
2026-02-27 00:29:14 -0700

    working submit guesses

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -6,11 +6,11 @@ import { Home } from '../home/home';
 export function Game(){
     const[possibilities, setPosb] = React.useState([]);
     const[scripture, setScrip] = React.useState('');
-    const [g] = React.useState(null);
+    const [guesses,setGuesses] = React.useState([]);
     React.useEffect(() =>{
         const ans = "James 1:5";
         setScrip("James 1:5\nIf any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him.\nJoseph Smith History 1:16 \nBut, exerting all my powers to call upon God to deliver me out of the power of this enemy which had seized upon me, and at the very moment when I was ready to sink into despair and abandon myself to destruction—not to an imaginary ruin, but to the power of some actual being from the unseen world, who had such marvelous power as I had never before felt in any being—just at this moment of great alarm, I saw a pillar of light exactly over my head, above the brightness of the sun, which descended gradually until it fell upon me.".split("\n")[1].split(" "));
-    });
+    },[]);
     async function saveScore(score){
         const date = new Date().toLocaleDateString();
         const newScore = {name: userName, score: score, date: date};
@@ -41,9 +41,9 @@ export function Game(){
 
     localStorage.setItem('scores', JSON.stringify(scores));
     }
-    const guesses = [];
     async function guess(str) {
-        guesses.push(<li>{str}</li>)
+        document.getElementById("guesser").reset();
+        setGuesses(prev => [...prev, str]);   
     }
     return(
         <div>
@@ -52,10 +52,12 @@ export function Game(){
     <h3 className = "in_game">You have successfully Identified the Scripture</h3>
     <h4 className = "in_game">Your guesses</h4>
     <ul className = "in_game">
-        <li>James 1:5</li>
+        {guesses.map((g, i) => (
+            <li key={i}>{g}</li>
+        ))}
     </ul>
-    <form className = "in_game" method = 'get' onSubmit={guess(g)}>
-        <input type = "text" placeholder = "Enter Guess..." value = {g}></input>
+    <form id = "guesser" className = "in_game" method = 'get' onSubmit={(e) => {e.preventDefault(), guess(e.target.elements[0].value)}}>
+        <input type = "text" placeholder = "Enter Guess..." ></input>
     </form>
     <h4 className = "in_game">
         Your Points: <span className = "points_disp">10</span>
