a8b2abaf68d73188d88e56aebf790eac38d4d6ef
cepsugar-del <cepsugar@gmail.com>
2026-02-26 22:37:01 -0700

    aaaaaaaaarg

--- index.jsx
+++ index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import {App} from './src/app';
+
+console.log("Made it into index.jsx");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
--- src/app.jsx
+++ src/app.jsx
@@ -1,53 +1,71 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Game } from './game/game';
 import { Home } from './home/home';
 import { AuthState } from './login/authState';
-function NotFound() {
-  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
-}
-export default function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+function App() {
   const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
   const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
   const [authState, setAuthState] = React.useState(currentAuthState);
   return (
     <BrowserRouter>
-    <div>
-  <Routes>
-  <Route 
-    path='/' 
-    element={
-      <Login 
-        userName={userName} 
-        authState={authState} 
-        onAuthChange={(userName,authState) => {
-          setAuthState(authState);
-          setUserName(userName);
-        }}
-      />
-    } 
-    exact 
-  />
-  <Route 
-    path='/login' 
-    element={
-      <Login 
-        userName={userName} 
-        authState={authState} 
-        onAuthChange={(userName,authState) => {
-          setAuthState(authState);
-          setUserName(userName);
-        }}
-      />
-    }
-  />
-  <Route path='/game' element={<Game />} />
-  <Route path='/home' element={<Home />} />
-  <Route path='*' element={<NotFound />} />
-</Routes>
-</div>
-    </BrowserRouter>);
+      <div>
+        <header>
+          <nav>
+          <menu>
+            <li className = 'nav-item'>
+              <NavLink className = 'nav-link' to = ''>
+                Login
+              </NavLink>
+            </li>
+            {authState === AuthState.Authenticated &&(
+              <li className = 'nav-item'>
+                <NavLink className = 'nav-link' to = 'game'>
+                  Play
+                </NavLink>
+              </li>
+            )}
+            {authState === AuthState.Authenticated &&(
+              <li className = 'nav-item'>
+                <NavLink className = 'nav-link' to = 'home'>
+                  Scores
+                </NavLink>
+              </li>
+            )}
+          </menu>
+          </nav>
+        </header>
+        <Routes>
+          <Route 
+            path='/' 
+            element={
+              <Login 
+                userName={userName} 
+                authState={authState} 
+                onAuthChange={(userName,authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            } 
+            exact 
+          />
+          <Route path='/game' element={<Game />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <footer>
+          <a href = "">Webworm</a>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
 }
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+}
+export default App;
