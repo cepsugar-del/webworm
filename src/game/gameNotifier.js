8e4805fa230d9f24b3767ecfe8a033ef5cca8ca6
cepsugar-del <cepsugar@gmail.com>
2026-04-08 01:26:10 -0600

    minor change

--- src/game/gameNotifier.js
+++ src/game/gameNotifier.js
@@ -19,7 +19,7 @@ class Notifier {
     constructor() {
         let port = window.location.port;
         const protocol = window.location.protocol === 'http:' ? 'ws':'wss';
-        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${4000}/ws`);
+        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
         this.socket.onopen = (event) =>{
             this.receiveEvent(new Msg('Startup',GameEvent.sys, {msg: 'connected'}));
         };
